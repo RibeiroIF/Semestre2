e0f71b8a477ea963ef83419672aac089c7a339c5
Gabriel <Gabriel.souza.0702@gmail.com>
2025-09-15 15:23:32 -0300

    Casa

--- Programação Web I/Lista 3/lista3.php
+++ Programação Web I/Lista 3/lista3.php
@@ -122,7 +122,7 @@
          <!-- Questão 4: Mesma questão que a 1, porém agora deverá ter uma aba de pesquisa pelo nome de um aluno qualquer, encontrando o nome dele na matriz já salva -->
         <div class="questao">
          <h2> Questao 4 </h2>
-         <form action="lista3.php" method="post" id="form4a">
+         <form action="lista3.php" method="post" id="form4">
             <fieldset>
                <legend> Informações do primeiro aluno: </legend>
                <label for="matricula1"> Matrícula: </label>
@@ -150,20 +150,17 @@
                <label for="media3"> Média Final: </label>
                <input type="number" name="media3" min="0" max="10" step="0.1"><br>
             </fieldset>
-         </form>
-         <button form="form4a" type="submit" class="button" name="submit_form4a"> Enviar </button>
-         <form action="lista3.php" method="post" id="form4b">
             <fieldset>
                <legend> Pesquisa por aluno : </legend>
                <label for="pesquisa"> Insira o nome do aluno que deseja pesquisar: </label>
                <input type="text" name="pesquisa">
             </fieldset>
          </form>
-         <button form="form4b" type="submit" class="button" name="submit_form4b"> Pesquisar </button>
+         <button form="form4" type="submit" class="button" name="submit_form4"> Pesquisar </button>
         </div>  
-         <!-- Questão 5: Mesma que a 2, porém agora organizando os alunos por média final de forma decrescente -->
+         <!-- Questão 5: Mesma que a 4, porém pesquisando pela matrícula do aluno -->
         <div class="questao">
-         <h2> Questao 2 </h2>
+         <h2> Questao 5 </h2>
          <form action="lista3.php" method="post" id="form5">
             <fieldset>
                <legend> Informações do primeiro aluno: </legend>
@@ -192,11 +189,47 @@
                <label for="media3"> Média Final: </label>
                <input type="number" name="media3" min="0" max="10" step="0.1"><br>
             </fieldset>
+            <fieldset>
+               <legend> Pesquisa por matrícula: </legend>
+               <label for="pesquisa"> Insira a matrícula do aluno que deseja pesquisar: </label>
+               <input type="text" name="pesquisa">
+            </fieldset>
          </form>
          <button form="form5" type="submit" class="button" name="submit_form5"> Enviar </button>
         </div> 
          <!-- Questão 6: -->
         <div class="questao">
+         <h2> Questao 6 </h2>
+         <form action="lista3.php" method="post" id="form6">
+            <fieldset>
+               <legend> Informações do primeiro aluno: </legend>
+               <label for="matricula1"> Matrícula: </label>
+               <input type="text" name="matricula1"><br>
+               <label for="nome1"> Nome: </label>
+               <input type="text" name="nome1"><br>
+               <label for="media1"> Média Final: </label>
+               <input type="number" name="media1" min="0" max="10" step="0.1"><br>
+            </fieldset>
+            <fieldset>
+               <legend> Informações do segundo aluno: </legend>
+               <label for="matricula2"> Matrícula: </label>
+               <input type="text" name="matricula2"><br>
+               <label for="nome2"> Nome: </label>
+               <input type="text" name="nome2"><br>
+               <label for="media2"> Média Final: </label>
+               <input type="number" name="media2" min="0" max="10" step="0.1"><br>
+            </fieldset>
+            <fieldset>
+               <legend> Informações do terceiro aluno: </legend>
+               <label for="matricula3"> Matrícula: </label>
+               <input type="text" name="matricula3"><br>
+               <label for="nome3"> Nome: </label>
+               <input type="text" name="nome3"><br>
+               <label for="media3"> Média Final: </label>
+               <input type="number" name="media3" min="0" max="10" step="0.1"><br>
+            </fieldset>
+         </form>
+         <button form="form6" type="submit" class="button" name="submit_form6"> Enviar </button>
         </div>
     </div>
 
@@ -301,10 +334,78 @@
       }
       if(isset($_POST['submit_form4'])){
       // ESPAÇO PARA A QUESTÃO 4, PESQUISAR PELO NOME DE UM ALUNO PRESENTE NA MATRIZ
+      $aluno1 = $_POST["nome1"];
+      $aluno2 = $_POST["nome2"];
+      $aluno3 = $_POST["nome3"];
 
+      $matric1 = $_POST["matricula1"];
+      $matric2 = $_POST["matricula2"];
+      $matric3 = $_POST["matricula3"];
+
+      $media1 = $_POST["media1"];
+      $media2 = $_POST["media2"];
+      $media3 = $_POST["media3"];
+
+      $matrizAlunos = [$matricula1 => [$aluno1, $media1],
+                       $matricula2 => [$aluno2, $media2],
+                       $matricula3 => [$aluno3, $media3]];
+
+      $alunoPesquisado = $_POST['pesquisa'];
+
+      foreach($matrizAlunos as $matricula => $vetorInterno)
+         {
+         $vetorAuxiliar[$matricula] = $vetorInterno[0];
+         }
+      $matriculaEncontrada = array_search($alunoPesquisado, $vetorAuxiliar);
+
+      if(!$matriculaEncontrada)
+         { 
+         die("<p> Caro usuário, o aluno pesquisado, de nome <span> $alunoPesquisado, </span> não foi encontrado na matriz de alunos. Aplicação encerrada! </p>");
+         }
+
+      $mediaProcurada = $matrizAlunos[$matriculaEncontrada][1];
+
+      echo "<p> Resultados da busca do aluno na matriz: <br>
+            Aluno procurado = <span> $alunoPesquisado </span> <br>
+            Matrícula deste aluno = <span> $matriculaEncontrada </span> <br>
+            Média de PRW1 = <span> $mediaProcurada </span> </p>"; 
       }
+
       if(isset($_POST['submit_form5'])){
-      // ESPAÇO PARA A QUESTÃO 5, MOSTRAR EM FORMATO TABULAR DESCENDENTE PELA NOTA
+      // ESPAÇO PARA A QUESTÃO 5, PESQUISAR PELA MATRÍCULA DE UM ALUNO PRESENTE NA MATRIZ
+      $aluno1 = $_POST["nome1"];
+      $aluno2 = $_POST["nome2"];
+      $aluno3 = $_POST["nome3"];
+
+      $matric1 = $_POST["matricula1"];
+      $matric2 = $_POST["matricula2"];
+      $matric3 = $_POST["matricula3"];
+
+      $media1 = $_POST["media1"];
+      $media2 = $_POST["media2"];
+      $media3 = $_POST["media3"];
+
+      $matrizAlunos = [$matricula1 => [$aluno1, $media1],
+                       $matricula2 => [$aluno2, $media2],
+                       $matricula3 => [$aluno3, $media3]];
+
+      $matriculaPesquisada = $_POST['pesquisa'];
+      $encontrou = array_key_exists($matriculaPesquisada, $matrizAlunos);
+
+      if($encontrou){ 
+         $aluno = $matrizAlunos[$matriculaPesquisada][0];
+         $media = $matrizAlunos[$matriculaPesquisada][1];
+         echo "<p> Resultados da busca da matrícula do aluno na matriz: <br>
+                   Matrícula procurada = <span> $matriculaPesquisada </span> <br>
+                   Nome deste aluno = <span> $aluno </span> <br>
+                   Média de PRW1 = <span> $media </span> </p>"; 
+         }
+      else{
+         echo "<p> Caro usuário, o aluno pesquisado, de matrícula <span> $matriculaPesquisada, </span> não foi encontrado na matriz de alunos. Aplicação encerrada! </p>";
+         }
+      }
+      if(isset($_POST['submit_form6'])){
+      // ESPAÇO PARA A QUESTÃO 6, ORGANIZAR EM FORMATO TABULAR ORDEM DESCRESCENTE
       $aluno1 = $_POST['nome1'];
       $aluno2 = $_POST['nome2'];
       $aluno3 = $_POST['nome3'];
@@ -320,11 +421,29 @@
       $matrizAlunos = [$matricula1 => [$aluno1, $media1], 
                        $matricula2 => [$aluno2, $media2], 
                        $matricula3 => [$aluno3, $media3]];
-
+      
+      foreach($matrizAlunos as $matricula => $vetorInterno){
+         $vetorAuxiliar[$matricula] = $vetorInterno[1];
       }
-      if(isset($_POST['submit_form6'])){
-      // ESPAÇO PARA A QUESTÃO 6
+      arsort($vetorAuxiliar);
 
+      echo "<table>
+            <caption> Rendimento semestral do aluno de Programação Web I </caption>
+            <tr>
+               <th> Matrícula do aluno </th>
+               <th> Nome do aluno </th>
+               <th> Média de PRW1 do aluno <th>
+            </tr>";
+
+         foreach($vetorAuxiliar as $matricula => $mediaPRW1){
+         $aluno = $matrizAlunos[$matricula][0];
+         echo "<tr>
+               <td> $matricula </td>
+               <td> $aluno</td>
+               <td> $mediaPRW1</td>
+               </tr>";
+         }
+         echo "</table>";
       }
       ?>
       </div>
